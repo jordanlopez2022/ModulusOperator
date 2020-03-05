e142aa81df72a0a795440b04308127f90b0854bf
LOPEZ <jordan.lopez.2022@student.lhschools.org>
2020-03-05 13:48:02 -0500

    lastDigitMethodCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,40 +5,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        keepContained();
+        lastDigit();
     }
-    public static void keepContained() {
-        int number = 357;
-        System.out.println((number%5)+1);
+    public static void lastDigit() {
+        int number = 2999;
         Scanner keyboard;
         keyboard = new Scanner(System.in);
 
-        System.out.println("Give Me A Positive Number");
+        System.out.println("Give Me A Positive Number 4 digits");
         number = keyboard.nextInt();
 
-        number = 1;
-        System.out.println((number%5)+1);
-
-        number = 2;
-        System.out.println((number%5)+1);
-
-        number = 3;
-        System.out.println((number%5)+1);
-
-        number = 4;
-        System.out.println((number%5)+1);
-
-        number = 5;
-        System.out.println((number%5)+1);
-
-        number = 6;
-        System.out.println((number%5)+1);
-
-        number = 7;
-        System.out.println((number%5)+1);
-
-        number = 8;
-        System.out.println((number%5)+1);
-
+        System.out.println("The last number is: "+ number%10);
     }
 }
